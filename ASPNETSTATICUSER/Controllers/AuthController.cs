1c90acf44926e725a694de3c46eae8c3db2845e9
Kapil Ghimire <kapilghimire.ghimire@gmail.com>
2019-07-01 21:32:52 +0530

    added authentication for static user

---
+++ ASPNETSTATICUSER/Controllers/AuthController.cs
@@ -0,0 +1,83 @@
+﻿using ASPNETSTATICUSER.Models;
+using ASPNETSTATICUSER.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ASPNETSTATICUSER.Controllers
+{
+
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        private IUserService _userService;
+
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [Route("signin")]
+        public IActionResult SignIn(string returnUrl = null)
+        {
+            return View(new SignInModel());
+        }
+
+        [Route("signin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SignIn(SignInModel model, string returnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                User user;
+                if (await _userService.ValidateCredentials(model.Username, model.Password, out user))
+                {
+                    await SignInUser(user.Username);
+                    // checking url is local prevents from redirection attacts
+                    if (!String.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    return RedirectToAction("Index", "Dashboard");
+                }
+            }
+            return View(model);
+        }
+
+        [Route("signout")]
+        [HttpGet]
+        public async Task<IActionResult> SignOut()
+        {
+
+            if (HttpContext.User != null)
+            {
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+                foreach (var cookieKey in Request.Cookies.Keys)
+                {
+                    Response.Cookies.Delete(cookieKey);
+                }
+
+            }
+
+            return Redirect("~/auth/signin");
+        }
+
+        public async Task SignInUser(string username)
+        {
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.NameIdentifier, username),
+                new Claim("name", username)
+            };
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme, "name", null);
+            var principal = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(principal);
+        }
+    }
+
+}
--- ASPNETSTATICUSER/Startup.cs
+++ ASPNETSTATICUSER/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNETSTATICUSER.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -13,19 +15,35 @@ namespace ASPNETSTATICUSER
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration,IHostingEnvironment hostingEnvironment)
         {
             Configuration = configuration;
+            HostingEnvironment = hostingEnvironment;
         }
 
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment HostingEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-          
+            services.AddSingleton<IUserService>(new UserService(this.getSystemUser()));
 
+            services.AddAuthentication(configureOptions =>
+            {
+                configureOptions.DefaultAuthenticateScheme= CookieAuthenticationDefaults.AuthenticationScheme;
+                configureOptions.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                configureOptions.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            }).AddCookie(options => {
+                options.LoginPath = "/auth/signin";
 
+                // SET SECURE FLAG ON COOKIE
+                options.Cookie.SecurePolicy = HostingEnvironment.IsDevelopment()? CookieSecurePolicy.None : CookieSecurePolicy.Always;
+                options.Cookie.SameSite = SameSiteMode.Strict;
+
+                options.ExpireTimeSpan = TimeSpan.FromHours(1);
+
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -38,13 +56,31 @@ namespace ASPNETSTATICUSER
             }
             else
             {
+                app.UseHttpsRedirection();
                 app.UseExceptionHandler("/Error");
+                
             }
 
             app.UseStaticFiles();
-           
 
-            app.UseMvc();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "Default",
+                template: "{controller=Dashboard}/{action=Index}");
+            });
+        }
+
+        public Dictionary<string, string> getSystemUser()
+        {
+            return new Dictionary<string, string>()
+            {
+                {"admin","admin"}
+            };
         }
     }
+
+    
 }
